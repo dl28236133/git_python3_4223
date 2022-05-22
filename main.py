f45232c77c8f33de040b85ec1a3826aaa91b0d93
MCshin00 <sinminc7@gmail.com>
2022-05-22 19:21:40 +0900

    회원 UI 생성

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+from member import *
 
 def main():
     window = Tk()
@@ -7,6 +7,20 @@ def main():
     window.geometry('800x600')
     window.configure(bg='LightSkyBlue1')
 
-    #수정이 됐나?
+    mainMenu = Menu(window)
+    window.config(menu = mainMenu)
+
+    memberMenu = Menu(mainMenu)
+    mainMenu.add_cascade(label = "회원관리", menu = memberMenu)
+    memberMenu.add_command(label="회원정보", command=member_search)
+    memberMenu.add_separator()
+    memberMenu.add_command(label="회원등록", command=member_register)
+    memberMenu.add_separator()
+    memberMenu.add_command(label="회원정보수정", command=member_search_fix)
+    memberMenu.add_separator()
+    memberMenu.add_command(label="회원탈퇴", command = member_search_del)
+    memberMenu.add_separator()
+    memberMenu.add_command(label="탈퇴회원확인", command = deleted_member_search)
+
     window.mainloop()
 main()
