879bb04da52d71bef9846f1ff4f2f457eda9964e
MCshin00 <sinminc7@gmail.com>
2022-05-31 21:45:52 +0900

    main 파일 업데이트

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from tkinter import *
-from member import *
 
-def main():
+from MEMBER import *
+from BOOK import *
+from RENT import *
+
+def main_UI():
     window = Tk()
     window.title('도서관리 프로그램')
     window.geometry('800x600')
@@ -10,17 +12,36 @@ def main():
     mainMenu = Menu(window)
     window.config(menu = mainMenu)
 
-    memberMenu = Menu(mainMenu)
-    mainMenu.add_cascade(label = "회원관리", menu = memberMenu)
-    memberMenu.add_command(label="회원정보", command=member_search)
-    memberMenu.add_separator()
-    memberMenu.add_command(label="회원등록", command=member_register)
-    memberMenu.add_separator()
-    memberMenu.add_command(label="회원정보수정", command=member_search_fix)
-    memberMenu.add_separator()
-    memberMenu.add_command(label="회원탈퇴", command = member_search_del)
-    memberMenu.add_separator()
-    memberMenu.add_command(label="탈퇴회원확인", command = deleted_member_search)
+    MemberMenu = Menu(mainMenu)
+    mainMenu.add_cascade(label = '회원관리' , menu = MemberMenu)
+    MemberMenu.add_command(label = '회원정보', command = member_search)
+    MemberMenu.add_command(label='회원등록', command=member_register)
+    MemberMenu.add_command(label='회원정보수정', command=member_search_fix)
+    MemberMenu.add_command(label='회원탈퇴', command=member_search_del)
+    MemberMenu.add_command(label='탈퇴회원확인', command=deleted_member_search)
+
+    BookMenu = Menu(mainMenu)
+    mainMenu.add_cascade(label='도서관리', menu=BookMenu)
+    BookMenu.add_command(label='새도서추가', command=Book_add)
+    BookMenu.add_command(label='책정보조회', command=Book_search)
+    BookMenu.add_command(label='책정보수정', command=bookfix_info)
+
+    RentMenu = Menu(mainMenu)
+    mainMenu.add_cascade(label='도서대여', menu=RentMenu)
+    RentMenu.add_command(label='도서대여', command=clickRentUser)
+    RentMenu.add_command(label='도서반납', command=clickReturnUser)
+
+    SettingMenu = Menu(mainMenu)
+    mainMenu.add_cascade(label='환경설정', menu=SettingMenu)
+    SettingMenu.add_command(label='실험', command=window.quit) # 나중에 지워야함
+    SettingMenu.add_command(label='종료', command= window.quit)
 
     window.mainloop()
-main()
+member_csv()
+main_UI()
+
+
+
+
+
+
